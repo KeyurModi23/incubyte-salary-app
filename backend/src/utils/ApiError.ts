4300d4ab474153f5f0b215b82ec76d23c18a3890
Your Name <keyur2309@gmail.com>
2026-09-22 14:46:59 +0530

    fix: commit missing api service and utility files

---
+++ backend/src/utils/ApiError.ts
@@ -0,0 +1,11 @@
+export class ApiError extends Error {
+  public statusCode: number;
+  public success: boolean;
+
+  constructor(statusCode: number, message = "Something went wrong") {
+    super(message);
+    this.statusCode = statusCode;
+    this.success = false;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
